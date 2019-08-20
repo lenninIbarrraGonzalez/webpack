ab366b985607427ca4d16620c5749afc380705cc
Lennin666 <soporte@allnexus.com.co>
2019-08-20 15:39:52 -0500

    Cargando configuraciones por defecto y personalizadas por medio del uso de la bandera --config podemos especificar un archivo de configuración externo con el nombre que queramos en lugar del nombre por defecto webpack.config.js.

--- external/webpack.config.js
+++ external/webpack.config.js
@@ -4,9 +4,11 @@ const path = require('path');
 //exportar el modulo
 
 module.exports = {
-  entry: './index.js',
-    output: {
-    path: path.resolve(__dirname),
+  entry: path.resolve(__dirname,'index.js'),
+  mode: 'development',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
     filename: 'bundle.js'
   }
 }
+
